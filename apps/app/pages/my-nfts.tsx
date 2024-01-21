9a9111961a39c3ab83c4e6f83c142ba19affbb38
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2024-01-21 16:31:18 +0530

    add skeleton

--- apps/app/pages/my-nfts.tsx
+++ apps/app/pages/my-nfts.tsx
@@ -4,13 +4,16 @@ import { CardContent, Card } from "@/components/ui/card";
 import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
 import NftViewer from "@components/NftViewer";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Component() {
+  const [fetching, setFetching] = useState(false);
   const { address } = useAccount();
   const [NFTsOwned, setNftsOwned] = useState<Array<Object>>();
 
   useEffect(() => {
     async function fetchData() {
+      setFetching(true);
       const response = await fetch(`/api/getNfts`, {
         method: "POST",
         headers: {
@@ -27,7 +30,14 @@ export default function Component() {
 
       console.log("nft data from /my-nfts", data);
     }
-    fetchData();
+
+    try {
+      fetchData();
+    } catch (error) {
+      console.error("Error fetching NFT data:", error);
+    } finally {
+      setFetching(false);
+    }
   }, [address]);
   return (
     <div key="1" className="flex flex-col min-h-screen">
@@ -38,14 +48,23 @@ export default function Component() {
             Please switch to the correct network to view your NFTs.
           </p> */}
           <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {NFTsOwned?.map((nft: any, index: number) => (
-              <NftViewer
-                key={index}
-                image={nft.image.originalUrl}
-                name={nft.name}
-                description={nft.description}
-              />
-            ))}
+            {fetching
+              ? new Array(8)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Skeleton
+                      className=" border-2 h-60 bg-gray-100 border-gray-400 p-2 rounded-2xl"
+                      key={index}
+                    />
+                  ))
+              : NFTsOwned?.map((nft: any, index: number) => (
+                  <NftViewer
+                    key={index}
+                    image={nft.image.originalUrl}
+                    name={nft.name}
+                    description={nft.description}
+                  />
+                ))}
           </div>
         </div>
       </main>
